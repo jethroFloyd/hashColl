58c088bd4dd2b2bacc8832603437a3cf284f4ca9
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-04-20 01:48:28 +0530

    Started Code

---
+++ sha1coll.c
@@ -0,0 +1,14 @@
+/*
+ *
+ * Author : Ritobroto Maitra
+ * SHA - 1 Cryptanalysis
+ *
+ */
+#ifndef i
+ 
+#define F(i)	
+ int main() {
+
+ }
+
+ A[i][j] = A[i-1][(j-5)%32]^A[i-5][(j-2)%32]^K[i][j]^W[i][j]^(F(i))
