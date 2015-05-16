2aa36098c953599ce468ad795f95eb57921e1cfc
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-05-17 03:20:47 +0530

    propagate not working, yet.

--- propagate.c
+++ propagate.c
@@ -13,6 +13,8 @@
 #include "sha2.h"
 #include <string.h>
 
+
+
 typedef struct bitStyle {
 
 	unsigned int bit1:1;
@@ -25,8 +27,11 @@ typedef struct bitPattern {
 	int count;
 	char name;
 } bitPattern;
-
+char choose (char, char, char, bitPattern *);
 int main() {
+
+
+	/* Defining the four different possibilities */
 
 	bitStyle ZZ;
 	ZZ.bit1 = 0;
@@ -44,9 +49,21 @@ int main() {
 	OO.bit1 = 1;
 	OO.bit2 = 1;
 
-	bitPattern patternMS[16]; /* Pattern Mother Set */
+	/* Pattern Mother Set */
+
+	bitPattern patternMS[16]; 
 	
 	
+	/**
+	 *
+	 *	Notation for Possible Generalized Conditions 
+	 *	To generate the following lines, use auxProp.c 
+	 *	Run: gcc auxProp.c -o auxProp.o && ./auxProp.o 
+	 * 	This creates a file called auxProp.txt in the directory. 
+	 *
+	 *
+	 */
+
 	patternMS[0].name='?';
 	patternMS[0].count=4;
 	patternMS[0].bits[0]=ZZ;
@@ -127,9 +144,12 @@ int main() {
 	patternMS[15].bits[1]=ZO;
 	patternMS[15].bits[2]=OO;
 
+	/* Dealing with the function 'choose' */
+	/* Test condition */
 	
+	char result = choose ('3', '5', '7', patternMS);
 
-
+	printf("\n%c\n", result);
 
 }
 
@@ -142,6 +162,183 @@ int main() {
  *	printf("'%d' \n ", abcd.bits[0].bit1); 
  *
  */
+
+char choose (char a, char b, char c, bitPattern *patternMS) {
+
+	/* 
+	 * We receive as parameters the differential conditions.
+  	 * For example : choose (x, u, n)
+  	 * We return a differential condition only.
+  	 *
+  	 */
+
+  	 int i,j,k,p,q,r;
+  	 for (i=0; i<16;i++) {
+  	 	if(patternMS[i].name == a)
+  	 		break;
+  	 }
+
+  	 for (j=0; j<16;j++) {
+  	 	if (patternMS[j].name == b)
+  	 		break;
+  	 }
+
+  	 for (k=0; k<16;k++) {
+  	 	if (patternMS[k].name == c)
+  	 		break;
+  	 }
+
+  	 int countA = patternMS[i].count;
+  	 int countB = patternMS[j].count;
+  	 int countC = patternMS[k].count;
+
+  	 bitPattern patternFind;
+  	 patternFind.name = '.';
+  	 patternFind.count = 0;
+
+  	 /* Consider the bit possibilities */
+  	 int bit1A = -1;
+  	 int bit2A = -1;
+
+  	 for (p=0; p<countA; p++) {
+  	 	if (p == 0) {
+  	 		
+  	 		bit1A = patternMS[i].bits[p].bit1;
+  	 		bit2A = patternMS[i].bits[p].bit2;
+  	 		continue;
+  	 	}
+
+  	 	if ((bit1A!= 2) && (patternMS[i].bits[p].bit1 != bit1A))
+  	 		bit1A = 2;
+
+		if ((bit2A!= 2) && (patternMS[i].bits[p].bit2 != bit2A))
+  	 		bit2A = 2;  	 	
+  	 }
+
+  	 int bit1F = -1;
+  	 int bit2F = -1;
+
+  	 /* Find possibilities for Bit 1 of result. */
+
+  	int s;
+  	if (bit1A == 0)
+  		s = k;
+  	else if (bit1A == 1)
+  		s = j;
+
+  	if( bit1A != 2){
+  	go: for (p=0; p<countC; p++) {
+
+  			if (bit1F == 2)
+  				break;
+
+  	 		if(p == 0) {
+  	 			bit1F = patternMS[s].bits[p].bit1;
+  	 			continue;
+  	 		}
+
+  	 		if (bit1F != patternMS[s].bits[p].bit1) {
+  	 			bit1F = 2;
+  	 			break;
+  	 		}
+
+  	 	}
+  	 }
+
+  	 if (bit1A == 2) {
+  	 		s = k;
+  	 			goto go;
+  	 		s = j;
+  	 			goto go;
+  	 }
+
+  	 /* Find same for Bit 2 of Result. */
+
+  	 if (bit2A == 0)
+  		s = k;
+  	else if (bit2A == 1)
+  		s = j;
+
+  	if( bit2A != 2){
+  	go2: for (p=0; p<countC; p++) {
+
+  			if (bit2F == 2)
+  				break;
+
+  	 		if(p == 0) {
+  	 			bit2F = patternMS[s].bits[p].bit2;
+  	 			continue;
+  	 		}
+
+  	 		if (bit2F != patternMS[s].bits[p].bit2) {
+  	 			bit2F = 2;
+  	 			break;
+  	 		}
+
+  	 	}
+  	 }
+
+  	if (bit2A == 2) {
+  	 		s = k;
+  	 			goto go2;
+  	 		s = j;
+  	 			goto go2;
+  	 }
+
+  	 /* Now let us populate the result field. */
+  	char ret;
+  	
+  	if (bit1F == 0) {
+  		if(bit2F == 0)	
+  			ret = '0';
+  		if(bit2F == 1)	
+  			ret = 'n';
+  		if(bit2F == 2)	
+  			ret = '5';
+  	}
+
+	if (bit1F == 1) {
+  		if(bit2F == 0)	
+  			ret = 'u';
+  		if(bit2F == 1)	
+  			ret = '1';
+  		if(bit2F == 2)	
+  			ret = 'A';
+  	}
+
+  	if (bit1F == 2) {
+  		if(bit2F == 0)	
+  			ret = '3';
+  		if(bit2F == 1)	
+  			ret = 'C';
+  		if(bit2F == 2)	
+  			ret = '?';
+  	}  	
+
+  	return ret;
+
+
+}
+
+int checkExist (bitPattern toCheck, bitStyle b) {
+	int i;
+	int check = 0;
+	for (i=0; i<toCheck.count; i++) {
+		//if (toCheck.bits[i] == b) // change this
+			check = 1;
+	}
+
+	return check;
+}
+
+
+
+
+
+
+
+
+
 
 
 	
