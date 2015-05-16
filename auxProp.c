1cd89648468dc13524aa66993bf1a9fa2c3dd455
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-05-17 01:26:21 +0530

    added auxilliary code for propagate

---
+++ auxProp.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "sha2.h"
+
+
+int main() {
+
+	int i,j;
+	char name, garbage;
+	int count;
+	char style[4][2];
+
+	FILE *fp;
+
+	fp=fopen("auxprop.txt","w");
+	fprintf(fp, "\n");
+
+	for (i=0;i<16;i++) {
+		scanf("%c", &garbage);
+		printf("\nRound %d:\n\n Enter Name:\n", i);
+		scanf("%c", &name);
+		fprintf(fp, "patternMS[%d].name='%c';\n", i, name);
+
+		printf("\nFor %c Enter Count:\n", name);
+		scanf("%d", &count);
+		fprintf(fp, "patternMS[%d].count=%d;\n", i, count);
+
+		for(j=0;j<count;j++) {
+			printf("\nEnter style %d:\n", j);
+			scanf("%s", style[j]);
+			fprintf(fp, "patternMS[%d].bits[%d]=%s;\n", i,j,style[j]);
+		}
+		
+		fprintf(fp, "\n");
+	}
+
+	fclose(fp);
+}
